cba597a5526acabdcfe5e06695031b990710ed16
Namit <pareeknamit8@gmail.com>
2026-02-23 20:42:26 +0530

    Initial Commit

---
+++ src/main/java/com/menamiit/smartcityproject/SmartCityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.menamiit.smartcityproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCityProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartCityProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/menamiit/smartcityproject/SmartCityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menamiit.smartcityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCityProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

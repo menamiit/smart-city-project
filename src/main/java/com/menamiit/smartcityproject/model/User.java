b3e1d718484fc9d246479d4aaaf74c7c0da85e5a
Namit <pareeknamit8@gmail.com>
2026-02-24 21:07:02 +0530

    Added model

---
+++ src/main/java/com/menamiit/smartcityproject/model/User.java
@@ -0,0 +1,29 @@
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Al1ArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role; // CITIZEN, ADMIN, OFFICER
+
+    private String email;
+    private String phone;
+}
